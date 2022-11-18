0c5de3baa14bcf7060d6f29fe49ac837841dea75
Daiki0917 <115197400+Daiki0917@users.noreply.github.com>
2022-11-18 09:27:29 +0900

    リモートリポジトリでコンフリクト変更

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -7,9 +7,7 @@ int main()
 {
     std::cout << "Hello GitHub World!\n";
 
-    std::cout << "Hello New branch \n";
-
-    std::cout << "third branch\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
